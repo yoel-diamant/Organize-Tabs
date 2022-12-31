3bd01c723de03a3306c33fd9cd3843c539079aee
Yoel Diamant <yoel_diamant@rapid7.com>
2022-12-31 18:38:49 +0200

    refactor

--- backround.js
+++ backround.js
@@ -1,151 +1,173 @@
+const ACTIONS = {
+    GROUP_BY_DOMAIN: 'group-by-domain',
+    UNGROUP: 'ungroup',
+    DELETE_DUPLICATE: 'delete-duplicate',
+    SORT_BY_DOMAIN: 'sort-by-domain',
+    SORT_BY_TITEL: 'sort-by-title',
+    SORT_BY_ID: 'sort-by-id',
+};
+
 function extractDomain(url) {
-    const singleWordTlds = ['com', 'net', 'org'];
+    const singleWordTlds = [
+        'com',
+        'net',
+        'org',
+    ];
+    let domain;
     const splitUrl = url.split('.');
     const suffix = splitUrl[splitUrl.length];
     if (splitUrl.length === 1) {
-        return url;
-    }
-    else if (singleWordTlds.includes(suffix)) {
-        return splitUrl[splitUrl.length - 1];
+        domain = url;
+    } else if (singleWordTlds.includes(suffix)) {
+        domain = splitUrl[splitUrl.length - 1];
     } else {
-        return splitUrl[splitUrl.length - 2];
-    }
+        domain = splitUrl[splitUrl.length - 2];
+    };
 
+    return domain;
+};
+
+async function getNormalTabs() {
+    return await chrome.tabs.query({});
 };
 
 async function groupByDomain() {
-    const tabs = await chrome.tabs.query({});
-
+    const tabs = await getNormalTabs();
     const tabGroups = {};
     tabs.forEach(tab => {
-        var url = new URL(tab.url)
-        var domain = extractDomain(url.hostname)
+        var url = new URL(tab.url);
+        var domain = extractDomain(url.hostname);
         const tabIds = tabGroups[domain];
         if (!tabIds) {
             tabGroups[domain] = [tab.id];
         } else {
             tabGroups[domain].push(tab.id);
-        }
+        };
 
     });
     for (const [url, tabIds] of Object.entries(tabGroups)) {
         if (tabIds.length > 1) {
             const group = await chrome.tabs.group({ tabIds });
             await chrome.tabGroups.update(group, { title: url });
-        }
-    }
-}
+        };
+    };
+};
+
 async function ungroup() {
-    const tabs = await chrome.tabs.query({});
-    const tabIds = tabs.map(({ id }) => id)
+    const tabs = await getNormalTabs();
+    const tabIds = tabs.map(({ id }) => id);
     chrome.tabs.ungroup(
         tabIds
-    )
+    );
 };
 
 async function deleteDuplicate() {
-    const tabs = await chrome.tabs.query({});
+    const tabs = await getNormalTabs();
     const tabGroups = {};
     tabs.forEach(tab => {
-        const url = new URL(tab.url)
+        const url = new URL(tab.url);
         const tabIds = tabGroups[url];
         if (!tabIds) {
             tabGroups[url] = tab.id;
         } else {
             chrome.tabs.remove(
                 [tab.id]
-            )
-        }
+            );
+        };
     });
 };
 
 async function sortByTitle() {
-    const tabs = await chrome.tabs.query({});
-    tabs.sort(
+    await sortTabsBy(
         (a, b) => {
-            return (a.title > b.title) ? 1 : (a.title === b.title) ? ((a.url > b.url) ? 1 : -1) : -1
-        }
+            return (a.title > b.title) ? 1 : (a.title === b.title) ? ((a.url > b.url) ? 1 : -1) : -1;
+        },
     );
+};
 
-    for (let index = 0; index < tabs.length; index++) {
-        await chrome.tabs.move(tabs[index].id, { index });
-    }
+async function sortById() {
+    await sortTabsBy(
+        (a, b) => {
+            return (a.id > b.id) ? 1 : -1;
+        },
+    );
 };
 
 async function sortByDomain() {
-    const tabs = await chrome.tabs.query({});
-    tabs.sort(
+    await sortTabsBy(
         (a, b) => {
-            return (extractDomain(a.url) > extractDomain(a.url)) ? 1 : (extractDomain(a.url) === extractDomain(a.url)) ? ((a.url > b.url) ? 1 : -1) : -1
-        }
+            return (extractDomain(a.url) > extractDomain(a.url)) ? 1 : (extractDomain(a.url) === extractDomain(a.url)) ? ((a.url > b.url) ? 1 : -1) : -1;
+        },
     );
+};
 
-    for (let index = 0; index < tabs.length; index++) {
-        await chrome.tabs.move(tabs[index].id, { index });
+async function sortTabsBy(byCallback) {
+    const tabs = await getNormalTabs();
+    tabs.sort(byCallback);
+
+    tabs.forEach(async (tab, index) => {
+        await chrome.tabs.move(tab.id, { index });
+    });
+}
+
+async function callEvent(
+    actionKey,
+) {
+    switch (actionKey) {
+        case ACTIONS.GROUP_BY_DOMAIN:
+            await groupByDomain();
+            break;
+        case ACTIONS.UNGROUP:
+            await ungroup();
+            break;
+        case ACTIONS.DELETE_DUPLICATE:
+            await deleteDuplicate();
+            break;
+        case ACTIONS.SORT_BY_TITEL:
+            await sortByTitle();
+            break;
+        case ACTIONS.SORT_BY_DOMAIN:
+            await sortByDomain();
+            break;
+        case ACTIONS.SORT_BY_ID:
+            await sortById();
+            break;
+        default:
+            console.error(`Event ${actionKey} not implemented`);
+            break;
     }
 };
 
-async function callEvent(
-    eventId,
-) {
-    switch (eventId) {
-        case 'group-by-domain':
-            await groupByDomain();
-            break;
-        case 'ungroup':
-            await ungroup();
-            break;
-        case 'delete-duplicate':
-            await deleteDuplicate();
-            break;
-        case 'sort-by-title':
-            await sortByTitle();
-            break;
-        case 'sort-by-domain':
-            await sortByDomain();
-            break;
-        default:
-            console.log(`Event ${eventId} not implemented`);
-            break;
-    }
-}
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-        id: 'group-by-domain',
-        title: 'Group By Domain',
-    });
-    chrome.contextMenus.create({
-        id: 'ungroup',
-        title: 'Ungroup All Tabs',
-    });
-    chrome.contextMenus.create({
-        id: 'sort-by-domain',
-        title: 'Sort By Domain',
-    });
-    chrome.contextMenus.create({
-        id: 'sort-by-title',
-        title: 'Sort By Title',
-    });
-    chrome.contextMenus.create({
-        id: 'delete-duplicate',
-        title: 'Delete Duplicate Tabs',
+    const contextMenus = {
+        [ACTIONS.GROUP_BY_DOMAIN]: 'Group By Domain',
+        [ACTIONS.UNGROUP]: 'Ungroup All Tabs',
+        [ACTIONS.SORT_BY_TITEL]: 'Sort By Title',
+        [ACTIONS.SORT_BY_DOMAIN]: 'Sort By Domain',
+        [ACTIONS.SORT_BY_ID]: 'Sort By Production Date',
+        [ACTIONS.DELETE_DUPLICATE]: 'Delete Duplicate Tabs',
+    };
+
+    Object.entries(contextMenus).forEach(([id, title]) => {
+        chrome.contextMenus.create({
+            id,
+            title,
+        });
     });
 });
 
 chrome.contextMenus.onClicked.addListener(async function (info, tab) {
-    const { menuItemId } = info
-    await callEvent(
-        menuItemId
-    )
+    const { menuItemId } = info;
+    await callEvent(menuItemId);
 });
 
 chrome.runtime.onMessage.addListener(async function (message, sender, sendResponse) {
     if (message) {
-        await callEvent(message)
+        await callEvent(message);
     }
-    return true
+    return true;
 });
 
 chrome.commands.onCommand.addListener(async (command) => {
-    await callEvent(command)
+    await callEvent(command);
 });
--- popup.js
+++ popup.js
@@ -1,24 +1,6 @@
-const buttonGroupBy = document.getElementById("group-by-button");
-buttonGroupBy.addEventListener("click", async function () {
-    chrome.runtime.sendMessage('group-by-domain');
-});
-
-const ungroupButton = document.getElementById("ungroup-button");
-ungroupButton.addEventListener("click", async function () {
-    chrome.runtime.sendMessage('ungroup');
-});
-
-const deleteDuplicateButton = document.getElementById("delete-duplicate-button");
-deleteDuplicateButton.addEventListener("click", async function () {
-    chrome.runtime.sendMessage('delete-duplicate');
-});
-
-const sortByTitle = document.getElementById("sort-by-title-button");
-sortByTitle.addEventListener("click", async function () {
-    chrome.runtime.sendMessage('sort-by-title');
-});
-
-const sortByDomain = document.getElementById("sort-by-domain-button");
-sortByDomain.addEventListener("click", async () => {
-    chrome.runtime.sendMessage('sort-by-domain');
-});
+const buttons = document.getElementsByClassName('action-button')
+for (let item of buttons) {
+    item.addEventListener("click", async function () {
+        chrome.runtime.sendMessage(item.id);
+    });
+};
